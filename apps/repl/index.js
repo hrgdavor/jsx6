d3c96e4e0d98d363bdfdb5aedb9a8bb4f0e5fc5a
Davor Hrg <hrgdavor@gmail.com>
2022-06-28 13:04:44 +0200

    babel standalone with jsx-simple

--- apps/repl/index.js
+++ apps/repl/index.js
@@ -1,4 +1,25 @@
 // https://github.com/stacktracejs/stacktrace-gps
 // https://github.com/stacktracejs/error-stack-parser
 
-console.log('repl')
+import { transform, availablePlugins } from "@babel/standalone";
+import jsx_mi2 from "babel-plugin-jsx-simple";
+import syntax_jsx from "babel-plugin-syntax-jsx";
+
+console.log((availablePlugins["jsx-mi2"] = jsx_mi2));
+console.log((availablePlugins["syntax-jsx"] = syntax_jsx));
+console.log( 
+  transform(
+    `import { h, Jsx6, appendChild } from '@jsx6/jsx6';
+     export const hello = <div>Hello {name}!</div>
+     `,
+    {
+      retainLines: true,
+      plugins: [
+        "syntax-jsx",
+        "jsx-mi2",
+        "syntax-object-rest-spread"
+      ],
+      presets: ["env"]
+    }
+  ).code
+);
