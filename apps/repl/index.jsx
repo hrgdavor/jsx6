26350bf7059f95bd53a53c768304a8ebeeee3174
Davor Hrg <hrgdavor@gmail.com>
2022-06-29 21:27:39 +0200

    lib packaging compat

--- apps/repl/index.jsx
+++ apps/repl/index.jsx
@@ -1,26 +1,5 @@
 // https://github.com/stacktracejs/stacktrace-gps
 // https://github.com/stacktracejs/error-stack-parser
 
-import { transform, availablePlugins } from '@babel/standalone'
-import { insertBefore } from '@jsx6/jsx6'
-import jsx_mi2 from 'babel-plugin-jsx-simple'
-import syntax_jsx from 'babel-plugin-syntax-jsx'
-
-const noEmpty = x => x
-
-insertBefore(document.body,<b>Hello world</b>)
-
-availablePlugins['jsx-mi2'] = jsx_mi2
-availablePlugins['syntax-jsx'] = syntax_jsx
-console.log(
-  transform(
-    `import { h, Jsx6, appendChild } from '@jsx6/jsx6'
-     export const hello = <div>Hello {name}!</div>
-     `,
-    {
-      retainLines: true,
-      plugins: ['syntax-jsx', 'jsx-mi2', 'syntax-object-rest-spread'],
-      presets: ['env'],
-    },
-  ).code,
-)
+import { appendChild, insertBefore } from '@jsx6/jsx6'
+appendChild(document.body,<b>Hello world.</b>)
--- libs/jsx6/src/insertBefore.js
+++ libs/jsx6/src/insertBefore.js
@@ -4,6 +4,7 @@ import { toDomNode } from './toDomNode'
 export function insertBefore(parent, newChild, before) {
   if (!parent) throwErr(ERR_REQUIRE_PARENT, { parent, newChild, before })
   const _parent = parent.insertBefore ? parent : toDomNode(parent)
+
   if (!_parent.insertBefore) console.error('missing insertBefore', _parent, parent)
   if (newChild.__init) {
     newChild.__init()
@@ -11,12 +12,29 @@ export function insertBefore(parent, newChild, before) {
   try {
     let _newChild = toDomNode(newChild)
     if (_newChild instanceof Array) _newChild = _newChild[0]
-
     _parent.insertBefore(_newChild, toDomNode(before))
-    if (newChild.insertSelf) newChild.insertSelf()
   } catch (error) {
     console.error('parent', parent, 'newChild', newChild, 'before', before)
     throw error
   }
   return newChild
+}
+
+export const appendChild = insertBefore
+
+export function replaceChild(parent, newChild, before) {
+  insertBefore(parent, newChild, before)
+  removeChild(parent, before)
+  return newChild
+}
+
+export function removeChild(parent, child) {
+  const _parent = parent.removeChild ? parent : toDomNode(parent)
+  try {
+    // if (_child instanceof Array) _child = _child[0]
+    _parent.removeChild(toDomNode(child))
+  } catch (error) {
+    console.error('parent', parent, 'newChild', newChild, 'before', before)
+    throw error
+  }
 }
