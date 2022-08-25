7aea714651f136a51bb8a3f28285d2693d91c65c
Davor Hrg <hrgdavor@gmail.com>
2022-08-25 13:17:32 +0200

    rush config for publish

---
+++ libs/jsx6/CHANGELOG.md
@@ -0,0 +1,9 @@
+# Change Log - @jsx6/jsx6
+
+This log was last generated on Thu, 25 Aug 2022 11:12:28 GMT and should not be manually modified.
+
+## 1.4.2
+Thu, 25 Aug 2022 11:12:28 GMT
+
+_Initial release_
+
--- libs/jsx6/src/observeResize.js
+++ libs/jsx6/src/observeResize.js
@@ -2,6 +2,15 @@ import { toDomNode } from './toDomNode'
 
 const listenMap = new WeakMap()
 
+/** Utility to handle observing resize events. Reason for having this is that
+ * observer calling back to js has performace overhead so it is preferred to reuse them.
+ *
+ * https://github.com/w3ctag/design-principles/issues/78
+ *
+ * @param {Node} el - DOM node to observe
+ * @param {Function} callback observer
+ * @returns {Function} a function that removes the observer
+ */
 export function observeResize(el, callback) {
   el = toDomNode(el)
   let arr = listenMap.get(el)
@@ -13,14 +22,11 @@ export function observeResize(el, callback) {
   if (!arr.length) observer.observe(el)
   arr.push(callback)
 
-  // function that removes the listener
+  // function that removes the observer
   return function () {
     const arr = listenMap.get(el)
     if (arr) {
-      const idx = arr.findIndex(f => f === callback)
-      if (idx != -1) {
-        arr.splice(idx, 1)
-      }
+      listenMap.set(el, (arr = arr.filter(f => f !== callback)))
     }
     if (!arr?.length) observer.unobserve(el)
   }
