062e9bf55ad44b0e571e78c170d4e7d4e86fb972
Davor Hrg <hrgdavor@gmail.com>
2022-08-28 16:01:13 +0200

    deve setup in README

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 JSX6
+
+# Development
+
+Befor you start you need 3 global modules
+- `npm install -g pnpm` - https://pnpm.io/ - npm alternative
+- `npm install -g rush` - https://rushjs.io/ - monorepo management
+- `npm install -g vite` - https://vitejs.dev/ - project runner and bundler
+- `npm install -g esbuild` - https://esbuild.github.io/ - superfast bundler compiler
+
+The project is not initialized using `npm` like it may be usual. It is a monoremo of multiple sub-projects
+so Rush was chosen to manage it.
+
+After cloning the repository you need to initialize  project.
+
+In the base dir of the project run `rush update`.
+
+
--- apps/repl/demistify.jsx
+++ apps/repl/demistify.jsx
@@ -1,13 +1,12 @@
 // https://github.com/stacktracejs/stacktrace-gps
 // https://github.com/stacktracejs/error-stack-parser
-
-import { insert, errTranslations, setTranslations } from '@jsx6/jsx6'
+import { errTranslations, insert, setTranslations } from '@jsx6/jsx6'
 import { textSeparatorForeground } from 'monaco-editor/esm/vs/platform/theme/common/colorRegistry'
 
 import styles from './editor.css'
+import { TutorialRunner } from './src/TutorialRunner'
 import { transformcjs } from './src/babel/transform'
 import { runCode } from './src/runner/simpleRunner'
-import { TutorialRunner } from './src/TutorialRunner'
 import { fetchText } from './src/util/fetchText'
 
 /** can be removed if wanting to cut a little bit on the output size, but get coded error messages */
