392f0f058823ab488ad623de89f3b890a187f7c6
Davor Hrg <hrgdavor@gmail.com>
2022-09-05 22:06:18 +0200

    version bumper

--- libs/editor-monaco/versync.js
+++ libs/editor-monaco/versync.js
@@ -1,13 +1,58 @@
 import fs from 'fs'
 
-fs.readFile('README.md', 'utf8', function (error, data) {
-  if (error) throw error
-  var bumped = data.replace(
-    /(!\[Version )(\d+\.\d+\.\d+)(\]\(https:\/\/img\.shields\.io\/badge\/version-)(\d+\.\d+\.\d+)(-gray\.svg\))/g,
-    process.argv[2] || '$1',
-  )
-  fs.writeFile('README.md', bumped, function (error) {
+const version = process.argv[2] || '0.0.0'
+const dryRun = process.argv[3] === 'dry'
+const doReplace = (file, regexArr) => {
+  if (file instanceof Array) {
+    file.forEach(file => doReplace(file, regexArr))
+    return
+  }
+
+  fs.readFile(file, 'utf8', function (error, data) {
+    console.log('parsing for version info ', file)
+
     if (error) throw error
-    console.log(process.argv[2] ? `Bumped readme file to ${process.argv[2]}` : `Version number not found.`)
+
+    let bumped = data
+
+    regexArr.forEach(rule => {
+      rule[1] = rule[1].replaceAll('[--VERSION--]', version)
+
+      if (dryRun) {
+        data.replace(rule[0], (...args) => {
+          let replaced = args[0].replace(...rule)
+          if (replaced === args[0]) replaced = '--UNCHANGED--'
+          console.log(`\n${args[0]}\n${replaced}\n`)
+        })
+      } else {
+        bumped = bumped.replace(...rule)
+      }
+    })
+
+    if (dryRun) return
+
+    fs.writeFile(file, bumped, function (error) {
+      if (error) throw error
+      if (bumped != data) {
+        console.log(
+          process.argv[2] ? `Bumped ${file} file to ${process.argv[2]}` : `Version number not found.`,
+        )
+      } else {
+        console.log('version not updated')
+      }
+    })
   })
-})
+}
+
+const unpkgDepRule = [/(\@jsx6\/editor\-monaco\@)(\d+\.\d+\.\d+)(\/)/g, `$1[--VERSION--]$3`]
+
+doReplace(
+  ['README.md', 'monaco.cdn.md', 'monaco.local.md'],
+  [
+    unpkgDepRule,
+    [
+      /(!\[Version )(\d+\.\d+\.\d+)(\]\(https:\/\/img\.shields\.io\/badge\/version-)(\d+\.\d+\.\d+)(-blue\.svg\))/g,
+      `$1[--VERSION--]$3[--VERSION--]$5`,
+    ],
+  ],
+)
