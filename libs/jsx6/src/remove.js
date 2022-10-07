2e3ab421023f0aa7843231408208ffc55f1204f8
Davor Hrg <hrgdavor@gmail.com>
2022-10-08 00:15:33 +0200

    feature replaceContent

--- libs/jsx6/src/remove.js
+++ libs/jsx6/src/remove.js
@@ -1,12 +1,10 @@
-import { destroy } from './destroy.js'
 import { toDomNode } from './toDomNode.js'
 
-export function remove(child, alsoDestroy) {
+export function remove(child) {
   try {
     const _child = toDomNode(child)
     const parent = _child.parentNode
     parent.removeChild(_child)
-    if (alsoDestroy) destroy(child)
   } catch (error) {
     console.error('parent', parent, 'child', child)
     throw error
