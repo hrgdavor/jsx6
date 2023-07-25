ab0404c9b3ae9882a7f510203e2504b7b33c5768
Davor Hrg <hrgdavor@gmail.com>
2023-07-25 11:46:08 +0200

    @jsx6/build 0.2.2 show output file name

--- tools/build/src/runEsbuild.js
+++ tools/build/src/runEsbuild.js
@@ -1,6 +1,6 @@
 import { existsSync } from 'fs'
 
-export const logBuildPlugin = {
+export const logBuildPlugin = output => ({
   name: 'buildTime',
   setup(build) {
     let buildStartTime
@@ -9,18 +9,18 @@ export const logBuildPlugin = {
     })
     build.onEnd(result => {
       if (!result.errors.length) {
-        console.log('build finished in ' + (Date.now() - buildStartTime) + 'ms')
+        console.log('build finished in ' + (Date.now() - buildStartTime) + 'ms\t ' + (output || ''))
       }
     })
   },
-}
+})
 
 export const runEsbuild = async (
   esbuild,
   { watch, dispose = true, skipExisting = false, ...esBuildOptions },
 ) => {
   if (!esBuildOptions.plugins) esBuildOptions.plugins = []
-  esBuildOptions.plugins.push(logBuildPlugin)
+  esBuildOptions.plugins.push(logBuildPlugin(esBuildOptions.outfile))
 
   const ctx = await esbuild.context(esBuildOptions)
 
